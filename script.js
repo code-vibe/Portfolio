7563c857cf1c2fcb4977855ccc09897ba84aabd8
code-vibe <mrsamoladapo@gmail.com>
2022-01-29 07:55:41 +0100

    Add code to script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// ADD ACTIVE CLASS ON  LINK CLICK
+
+const navUL = document.getElementById("nav-ul")
+const navLi = document.getElementByClassName("nav-li")
+
+for (var i = 0; i < navLi.length; i++) {
+	navLi[i].addEventListener
+}
