3f51c7676f302fdf02e24aba7c9e1dc89f84a98c
code-vibe <mrsamoladapo@gmail.com>
2022-01-29 09:37:57 +0100

    Add animation to nav-li class

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ const navUL = document.getElementById("nav-ul")
 const navLi = document.getElementByClassName("nav-li")
 
 for (var i = 0; i < navLi.length; i++) {
-	navLi[i].addEventListener
+	navLi[i].addEventListener("click", () => {
+		const current = document.getElementByClassName("active");
+		current[0].className = current[0].className.replace("active", "");
+		this.className += "active";
+	});
 }
